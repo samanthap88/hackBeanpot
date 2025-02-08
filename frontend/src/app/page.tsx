5579f2910c057cc2895fc44c716418e30eb645dc
samantha <samanthap8675@gmail.com>
2025-02-08 14:09:18 -0500

    onboarding form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,12 +4,13 @@
 import Image from "next/image";
 import { useState } from "react";
 import LandingPage from "./landing-page/page";
+import EnhancedOnboardingWidget from "./form/page";
 
 export default function Home() {
   return (
     
     <div>
-      <LandingPage/>
+      <EnhancedOnboardingWidget/>
     </div>
   );
 }
